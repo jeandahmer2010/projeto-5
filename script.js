5b6d7f71bd4eb797ee6b158e2438c93aa3c629f7
jeandahmer2010 <jean.dahmer.pereira@escola.pr.gov.br>
2025-08-14 08:47:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//vetores de Sabores
+
+const Sabores = [
+ {id: "pastel de frango", nome: "Pastel de frango", preco: 3,99 },   
+ {id: "pastel de Carne", nome: "Pastel de Carne", preco: 3,99 },
+ {id: "pastel de presunto e queijo", nome: "Pastel de queijo", preco: 3,99 },
+ {id: "pastel de pizza", nome: "Pastel de pizza", preco: 3,99 },
+             
+];
